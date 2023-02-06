0bd9e668356b8be85061e066df1efe8f972d7b1c
lukaszkus <kusiu@vp.pl>
2023-02-06 14:53:01 +0100

    next.js & tailwindcss basic configuration

---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <div className="sticky flex top-0 z-40 w-full h-24 bg-zinc-900">
+      <div className="flex justify-between w-full h-full max-w-7xl m-auto px-4">
+        <Link href="/">
+          <div className="flex items-center h-full cursor-pointer">
+            <div className="invisible md:visible">
+              <Image
+                width="150"
+                height="25"
+                src="/movie_db_logo.svg"
+                alt="Movie_DB logo"
+              />
+            </div>
+            <div className="absolute md:invisible">
+              <Image
+                width="80"
+                height="25"
+                src="/movie_db_logo_sm.svg"
+                alt="Movie_DB logo small"
+              />
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const queryClient = new QueryClient();
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
